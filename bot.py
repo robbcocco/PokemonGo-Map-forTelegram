191ed6fe3240c002f459e7e4753100d5704a81da
robbcocco <robertococco@outlook.com>
2016-07-22 16:58:20 +0200

    Updated to use master branch of Pokemap

--- bot.py
+++ bot.py
@@ -24,30 +24,33 @@ def handle(msg):
 
     if command.lower().startswith('/pokemap'):
         if command.count(' ') >= 1:
-            # printing info
+            # print info
             print('Sender: ', sender)
             print('Command: ', command)
             print('Msg date: ', msg_date)
             print('Act date: ', actual_date)
-            if msg_date+1 > actual_date:
-                # saving the location into a variable
+            # avoid old messages
+            if actual_date-msg_date < 3:
+                # save the location into a variable
                 locTemp = command.split(' ', 1)
                 location = locTemp[1]
-                # running the shell command
-                os.system('python PokemonGo-Map-develop/runserver.py -a ptc -u %s -p %s -l "%s" -st %s -H %s -P %s >mapstd.txt 2>maperr.txt &' % (USER, PASS, location, STEP, HOST, PORT))
+                # run the shell command
+                #os.system('python2.7 PokemonGo-Map-develop/runserver.py -a ptc -u %s -p %s -l "%s" -st %s -H %s -P %s >mapstd.txt 2>maperr.txt &' % (USER, PASS, location, STEP, HOST, PORT))
+                os.system('python2.7 PokemonGo-Map-master/example.py -a ptc -u %s -p %s -l "%s" -st %s -H %s -P %s >mapstd.txt 2>maperr.txt &' % (USER, PASS, location, STEP, HOST, PORT))
                 # let the map load a minute
                 bot.sendMessage(chat_id, 'Wait a minute...')
                 time.sleep(60)
-                # initializing the page
+                # initialize the page
                 driver = webdriver.PhantomJS()
                 driver.set_window_size(1024, 1024)
                 driver.get('http://%s:%s' % (HOST, PORT))
                 # let the page load
-                time.sleep(5)
+                time.sleep(3)
                 # save a screenshot
                 driver.save_screenshot('loc.png')
                 # kill the map
-                os.system('pkill -f runserver.py')
+                #os.system('pkill -f runserver.py')
+                os.system('pkill -f example.py')
                 os.system('pkill -f node')
                 os.system('pkill -f phantomjs')
                 # send the screenshot
@@ -57,14 +60,6 @@ def handle(msg):
                 bot.sendMessage(chat_id, 'I\'m now avaiable')
         else:
             bot.sendMessage(chat_id, 'Correct syntax is "/pokemap location"')
-
-    elif command.lower().startswith('/start'):
-        bot.sendMessage(chat_id, 'Hi!')
-
-    elif command.lower().startswith('/help'):
-        bot.sendMessage(chat_id,    'To get the map of a location with nearby Pokémon, just type\n' \
-                                    '/pokemap followed by the desired location\n\n' \
-                                    'Sources can be found here https://github.com/robbcocco/PokemonGo-Map-forTelegram', disable_web_page_preview=True)
 
 
 TOKEN = sys.argv[1]  # get token from command-line
