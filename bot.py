fb93632450eb022ae8b203d0f9af2bd73d63f6c8
robbcocco <robertococco@outlook.com>
2016-07-26 18:03:31 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,69 +1,77 @@
 import sys
-import time
+import asyncio
 import telepot
+import telepot.aio
 import os
 import datetime
+import time
 
 from selenium import webdriver
 
 """
 Run with
-$ python3.4 bot.py <token> <PTC username> <PTC password> <steps> <host> <port> <gmaps key>
+python3.5 bota.py <token> <PTC username> <PTC password> <steps> <host> <port> <gmaps key>
 
 """
 
-def handle(msg):
-    actual_date = time.time()
-    flavor = telepot.flavor(msg)
+async def handle(msg):
+    global server_used
     chat_id = msg['chat']['id']
     command = msg['text']
     sender = msg['from']
-    msg_date = msg['date']
-
-    summary = telepot.glance(msg, flavor=flavor)
-
     if command.lower().startswith('/pokemap'):
         if command.count(' ') >= 1:
             # print info
             print('Sender: ', sender)
             print('Command: ', command)
-            print('Msg date: ', msg_date)
-            print('Act date: ', actual_date)
-            # avoid old messages
-            if actual_date-msg_date < 3:
+            # check if the server is being used
+            if server_used is False:
+                # set the server as occupied
+                server_used = True
                 # save the location into a variable
                 locTemp = command.split(' ', 1)
                 location = locTemp[1]
                 # run the shell command
-                #os.system('python2.7 PokemonGo-Map-develop/runserver.py -a ptc -u %s -p %s -l "%s" -st %s -H %s -P %s >mapstd.txt 2>maperr.txt &' % (USER, PASS, location, STEP, HOST, PORT))
                 os.system('python2.7 PokemonGo-Map-1.0/example.py -a ptc -u %s -p %s -l "%s" -st %s -H %s -P %s >mapstd.txt 2>maperr.txt &' % (USER, PASS, location, STEP, HOST, PORT))
                 # let the map load a minute
-                bot.sendMessage(chat_id, 'Wait a minute...')
-                time.sleep(60)
+                await bot.sendMessage(chat_id, 'Wait a minute...')
+                await asyncio.sleep(60)
                 # initialize the page
                 driver = webdriver.PhantomJS()
                 driver.set_window_size(1024, 1024)
                 driver.get('http://%s:%s' % (HOST, PORT))
                 # let the page load
-                time.sleep(3)
+                await asyncio.sleep(3)
                 # save a screenshot
                 driver.save_screenshot('loc.png')
                 # kill the map
-                #os.system('pkill -f runserver.py')
                 os.system('pkill -f example.py')
                 os.system('pkill -f node')
                 os.system('pkill -f phantomjs')
                 # send the screenshot
-                bot.sendChatAction(chat_id, 'upload_photo')
-                bot.sendPhoto(chat_id, open('loc.png', 'rb'), caption=location)
+                await bot.sendChatAction(chat_id, 'upload_photo')
+                await bot.sendPhoto(chat_id, open('loc.png', 'rb'), caption=location)
+                # set the server as free
+                server_used = False
             else:
-                bot.sendMessage(chat_id, 'I\'m now avaiable')
+                await bot.sendMessage(chat_id, 'Wait till i\'m avaiable')
+                # wait until the server is free
+                while(server_used is True):
+                    await asyncio.sleep(1)
+                await bot.sendMessage(chat_id, 'I\'m now avaiable')
         else:
-            bot.sendMessage(chat_id, 'Correct syntax is "/pokemap location"')
+            await bot.sendMessage(chat_id, 'Correct syntax is "/pokemap location"')
+
+    elif command.lower().startswith('/start'):
+        await bot.sendMessage(chat_id, 'Hi! Try me with /pokemap')
+
+    elif command.lower().startswith('/help'):
+        await bot.sendMessage(chat_id,  'To get the map of a location with nearby Pokémon, just type\n' \
+                                        '/pokemap followed by the desired location')
 
 
-TOKEN = sys.argv[1]  # get token from command-line
-
+# get arguments from command-line
+TOKEN = sys.argv[1]
 USER = sys.argv[2]
 PASS = sys.argv[3]
 STEP = sys.argv[4]
@@ -71,9 +79,13 @@ HOST = sys.argv[5]
 PORT = sys.argv[6]
 #GKEY = sys.argv[7]
 
-bot = telepot.Bot(TOKEN)
-bot.message_loop(handle)
+# baw
+server_used = False
 
-# Keep the program running.
-while 1:
-    time.sleep(10)
+bot = telepot.aio.Bot(TOKEN)
+loop = asyncio.get_event_loop()
+
+loop.create_task(bot.message_loop(handle))
+print('Listening ...')
+
+loop.run_forever()
